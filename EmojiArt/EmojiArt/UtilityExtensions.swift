86cd42a894c8633071487f8cbfa0122909832942
Jason Stelzel <laptop@epigraf.com>
2022-08-18 01:00:11 -0700

    Add CGRect .center extension and place first emojis in document

--- EmojiArt/EmojiArt/UtilityExtensions.swift
+++ EmojiArt/EmojiArt/UtilityExtensions.swift
@@ -39,3 +39,12 @@ extension RangeReplaceableCollection where Element: Identifiable {
         }
     }
 }
+
+
+// Geometry extensions
+
+extension CGRect {
+    var center: CGPoint {
+        CGPoint(x: midX, y: midY)
+    }
+}
