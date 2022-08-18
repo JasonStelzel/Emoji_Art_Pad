b3a4f2ffee5c53d839713bcd99984168a867162d
Jason Stelzel <laptop@epigraf.com>
2022-08-18 01:52:36 -0700

    Add drag and drop functions from palette to document and loadObjects extensions on Array to NSItemProvider

--- EmojiArt/EmojiArt/UtilityExtensions.swift
+++ EmojiArt/EmojiArt/UtilityExtensions.swift
@@ -41,6 +41,18 @@ extension RangeReplaceableCollection where Element: Identifiable {
 }
 
 
+extension Character {
+    var isEmoji: Bool {
+        //Swift does not have a way to ask if a CHaracter isEmoji but it does let us check to see if our componenent scalars isEmoji.  Unfortunately, Unicode allows certain scalars (like 1) to be modified by another scalar to become emoji (e.g. 1️⃣) so the scalar "1" will report isEmoji = true so we can't just check to see if the first scalar isEmoji. The quick and dirty here is to see if the scalar is at least the first true emoji we know of (the start of the "miscellaneious items" section) or check to see if this is a multiple scalar unicode sequence (e.g. a 1 with a unicode modifier to force it to be presented as emoji 1️⃣)
+        if let firstScalar = unicodeScalars.first, firstScalar.properties.isEmoji {
+            return (firstScalar.value >= 0x238d || unicodeScalars.count > 1)
+        } else {
+            return false
+        }
+    }
+}
+
+
 // Geometry extensions
 
 extension CGRect {
@@ -48,3 +60,36 @@ extension CGRect {
         CGPoint(x: midX, y: midY)
     }
 }
+
+
+// Convenience functions for [NSItemProvider] (i.e. array of NSItemProvider) makes the code for loading objects from the providers a bit simpler. NSItemProvider is a holdover from the Obejctive-C world (i.e. pre-Swift) as indicated by its "NS" style prefix (from NextStep, the company founded by Steve Jobs when he left Apple at one point).
+
+extension Array where Element == NSItemProvider {
+    func loadObjects<T>(ofType theType: T.Type, firstOnly: Bool = false, using load: @escaping (T) -> Void) -> Bool where T: NSItemProviderReading {
+        if let provider = first(where: { $0.canLoadObject(ofClass: theType) }) {
+            let _ = provider.loadObject(ofClass: theType) {object, error in
+                if let value = object as? T {
+                    DispatchQueue.main.async {
+                        load(value)
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+    func loadObjects<T>(ofType theType: T.Type, firstOnly: Bool = false, using load: @escaping (T) -> Void) -> Bool where T: _ObjectiveCBridgeable, T._ObjectiveCType: NSItemProviderReading {
+        if let provider = first(where: { $0.canLoadObject(ofClass: theType) }) {
+            let _ = provider.loadObject(ofClass: theType) {object, error in
+                if let value = object {
+                    DispatchQueue.main.async {
+                        load(value)
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+}
+
